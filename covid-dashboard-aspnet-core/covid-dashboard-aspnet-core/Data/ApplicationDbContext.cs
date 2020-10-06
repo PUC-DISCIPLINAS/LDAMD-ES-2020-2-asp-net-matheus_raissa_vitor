d9c445f6bced2fc4ceb85a971a060ebbb8f821db
Matheus Santos <mcarneiro@sga.pucminas.br>
2020-10-06 14:13:12 -0300

    feat: added dbset for models

--- covid-dashboard-aspnet-core/covid-dashboard-aspnet-core/Data/ApplicationDbContext.cs
+++ covid-dashboard-aspnet-core/covid-dashboard-aspnet-core/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using covid_dashboard_aspnet_core.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,9 @@ namespace covid_dashboard_aspnet_core.Data
             : base(options)
         {
         }
+
+        public DbSet<DataCovid> DataCovid { get; set; }
+        public DbSet<Country> Country { get; set; }
+
     }
 }
