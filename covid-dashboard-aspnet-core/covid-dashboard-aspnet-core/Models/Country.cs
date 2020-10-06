7e76e2c8eb90ffff01f3050e0f450e9c659a9c6a
Matheus Santos <mcarneiro@sga.pucminas.br>
2020-10-06 14:08:37 -0300

    feat: added models

---
+++ covid-dashboard-aspnet-core/covid-dashboard-aspnet-core/Models/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace covid_dashboard_aspnet_core.Models
+{
+    [Table("countries")]
+    public class Country
+    {
+        [Key] // pk com auto incremento
+        [Display(Name="Código")] // maneira com que vai aparecer na view
+        public int Id { get; set; }
+
+        [Display(Name="Nome")]
+        [Required(ErrorMessage="Campo Obrigatório")]
+        public string Name { get; set; }
+
+        [Display(Name="Dados")]
+        public DataCovid DataCovid { get; set; }
+
+    }
+}
---
+++ covid-dashboard-aspnet-core/covid-dashboard-aspnet-core/Models/DataCovid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace covid_dashboard_aspnet_core.Models
+{
+    [Table("datas")]
+    public class DataCovid
+    {
+        [Key]
+        [Display(Name="Código")]
+        public int Id { get; set; }
+        
+        [Display(Name="Casos Confirmados")]
+        [Required(ErrorMessage="Campo Obrigatório")]
+        public int ConfirmedCase { get; set; }
+
+        [Display(Name="Mortes")]
+        [Required(ErrorMessage="Campo Obrigatório")]
+        public int Death { get; set; }
+
+        [Display(Name="Recuperados")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public int Recovered { get; set; }
+
+        [Display(Name="País")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public int CountryId { get; set; }
+
+        [ForeignKey("CountryId")]
+        [Display(Name="País")]
+        public Country Country { get; set; }
+
+    }
+}
