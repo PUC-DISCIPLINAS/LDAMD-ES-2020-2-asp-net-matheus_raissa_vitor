250e4eae2ce628828d53142c292819f92d3b0503
Matheus Santos <mcarneiro@sga.pucminas.br>
2020-10-06 14:21:17 -0300

    feat: added first migration

---
+++ covid-dashboard-aspnet-core/covid-dashboard-aspnet-core/Data/Migrations/20201006171447_first.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace covid_dashboard_aspnet_core.Data.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "datas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ConfirmedCase = table.Column<int>(nullable: false),
+                    Death = table.Column<int>(nullable: false),
+                    Recovered = table.Column<int>(nullable: false),
+                    CountryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_datas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_datas_countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_datas_CountryId",
+                table: "datas",
+                column: "CountryId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "datas");
+
+            migrationBuilder.DropTable(
+                name: "countries");
+        }
+    }
+}
